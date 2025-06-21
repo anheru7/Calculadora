58916ac21634baea5c830367cc797d0690cc19d6
anheru7 <31142339+anheru7@users.noreply.github.com>
2025-06-21 17:26:32 -0500

    Update CalculadoraSalarialController.cs

--- Calculadora-Salarial/Controllers/CalculadoraSalarialController.cs
+++ Calculadora-Salarial/Controllers/CalculadoraSalarialController.cs
@@ -31,7 +31,7 @@ namespace CalculadoraSalarial.Controllers
             }
             catch (Exception ex)
             {
-                return StatusCode(500, $"Error interno do servidor: {ex.Message}");
+                return StatusCode(500, $"Internal Server Error: {ex.Message}");
             }
         }
     }
